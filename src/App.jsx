5ce40fad5b0acc70fd54e5b627f372d14e37050d
ayushmanKushwah <ayushmansinghkushwah191203@gmail.com>
2024-12-14 18:41:10 +0530

    removed navbar jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import Navbar from "./components/navbar"
 import hero from "./assets/images/hero.png"
-
+import logo from "./assets/images/logo.png"
 function App() {
 
   return (
     <div>
-      <Navbar />
+      <nav className="navbar">
+      <div className='nav-logo'>
+        <img src={logo} />
+      </div>
+
+      <div className='nav-links'>
+        <p className='nav-links-sample'>What we do</p>
+        <p className='nav-links-sample'>What we think</p>
+        <p className='nav-links-sample'>Who we are</p>
+        <div className='nav-contact'>
+          <p className='nav-links-sample'>
+            Follow us
+          </p>
+          <p className='nav-icons'>
+            <i class="fa-brands fa-x-twitter"></i>
+            <i class="fa-brands fa-linkedin"></i>
+            <i class="fa-brands fa-instagram"></i>
+          </p>
+        </div>
+      </div>
+    </nav>
       <div className="hero">
         <div className="hero-title">
           AI/ML solution to Elevate your customer Trust and
